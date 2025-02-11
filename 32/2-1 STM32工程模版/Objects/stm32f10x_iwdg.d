a3f57984ea61d9f475fd75140fd86ad1ad04be91
shuai00115 <shuai00115@163.com>
2025-02-11 21:30:19 +0800

    编译2-1工程

--- 32/2-1 STM32工程模版/Objects/stm32f10x_iwdg.d
+++ 32/2-1 STM32工程模版/Objects/stm32f10x_iwdg.d
@@ -2,7 +2,7 @@
 .\objects\stm32f10x_iwdg.o: Library\stm32f10x_iwdg.h
 .\objects\stm32f10x_iwdg.o: .\Start\stm32f10x.h
 .\objects\stm32f10x_iwdg.o: .\Start\core_cm3.h
-.\objects\stm32f10x_iwdg.o: D:\Develop\keil5\Keil_V5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\stm32f10x_iwdg.o: D:\develop\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\stm32f10x_iwdg.o: .\Start\system_stm32f10x.h
 .\objects\stm32f10x_iwdg.o: .\User\stm32f10x_conf.h
 .\objects\stm32f10x_iwdg.o: .\Library\stm32f10x_adc.h
