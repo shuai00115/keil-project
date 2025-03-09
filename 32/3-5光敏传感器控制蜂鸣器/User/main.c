1152035022856443344df40f22e7d6a617e70409
shuai00115 <shuai00115@163.com>
2025-03-09 16:33:10 +0800

    更改工程3-5

--- 32/3-5光敏传感器控制蜂鸣器/User/main.c
+++ 32/3-5光敏传感器控制蜂鸣器/User/main.c
@@ -10,6 +10,13 @@ int main(void)
 	Buzzer_Init();
 	while(1)
 	{
-		
+		Buzzer_ON();
+		Delay_ms(500);
+		Buzzer_OFF();
+		Delay_ms(500);
+		Buzzer_Turn();
+		Delay_ms(500);
+		Buzzer_Turn();
+		Delay_ms(500);
 	}
 }
