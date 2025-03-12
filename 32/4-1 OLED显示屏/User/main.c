f53c3733ab3ad7b5c89a7fbbf382d5eed9b72572
shuai00115 <shuai00115@163.com>
2025-03-12 16:48:22 +0800

    更改工程4-1

--- 32/4-1 OLED显示屏/User/main.c
+++ 32/4-1 OLED显示屏/User/main.c
@@ -2,10 +2,20 @@
 #include "Delay.h"
 #include "LED.h"
 #include "Key.h"
+#include "OLED.h"
 
 int main(void)
 {
+	OLED_Init();
 	
+	OLED_ShowChar(1,1,'A');
+	
+	OLED_ShowString(1,3,"Hello World!");
+	OLED_ShowNum(2,1,123456,6);
+	OLED_ShowHexNum(3,1,0xAA55,4);
+	OLED_ShowBinNum(4,1,0xAA55,16);
+	
+	//OLED_Clear();
 	while(1)
 	{
 		
