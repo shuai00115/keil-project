569cf6bd2ae307922298908d93a8c2faa4964cdf
shuai00115 <shuai00115@163.com>
2025-03-18 20:21:59 +0800

    添加工程5-1

---
+++ 32/5-1 对射式红外传感器计次/User/main.c
@@ -0,0 +1,19 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "LED.h"
+#include "Key.h"
+#include "OLED.h"
+#include "CountSensor.h"
+
+int main(void)
+{
+	OLED_Init();
+	CountSensor_Init();
+	
+	OLED_ShowString(1,1,"Count:");
+	
+	while(1)
+	{
+		OLED_ShowNum(1,7,CountSensor_Get(),5);
+	}
+}
